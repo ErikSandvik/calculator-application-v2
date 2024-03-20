92f716b81d0fd65eae0c33a309a7f7ead9ac8478
erik <erisandv@stud.ntnu.no>
2024-03-21 00:46:15 +0100

    Working?

--- src/services/CalculatorService.js
+++ src/services/CalculatorService.js
@@ -2,32 +2,20 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:8081/api/calculator/';
 
-// Create an Axios instance to use for the calculator API
-const calculatorApi = axios.create({
-    baseURL: API_BASE_URL
-});
-
-// Add a request interceptor to the Axios instance
-calculatorApi.interceptors.request.use(request => {
-    console.log('Starting Request', JSON.stringify(request, null, 2));
-    return request;
-});
-
 export default {
-    calculate(number1, number2, operation) {
-        // Retrieve the token from localStorage
+    calculate(operation, number1, number2) {
+        // Retrieve the JWT token from local storage
         const token = localStorage.getItem('token');
 
-        // Ensure the token is set in the Authorization header for this request
+        // Include the token in the request headers
         const headers = {
             'Authorization': `Bearer ${token}`
         };
 
-        // Use the Axios instance with the interceptor to make the request
-        return calculatorApi.post('/calculate', {
+        return axios.post(`${API_BASE_URL}calculate`, {
+            operation,
             number1,
-            number2,
-            operation
-        }, { headers });
+            number2
+        }, { headers: headers });
     }
 };
